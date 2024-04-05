6b222669c5e38d074adb2f6e0ee97a9edae0863d
Lixtas <nguyenthekhiem148@gmail.com>
2024-04-05 22:13:27 +0700

    Update Story and Notification

--- app/src/main/java/com/example/instagram_demo/Component/notification.kt
+++ app/src/main/java/com/example/instagram_demo/Component/notification.kt
@@ -7,15 +7,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.instagram_demo.R
+
 @Composable
 fun Notification(){
     TopAppBar(
@@ -31,6 +34,12 @@ fun Notification(){
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(imageVector = Icons.Filled.FavoriteBorder,
                     contentDescription = null)
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Filled.Notifications,
+                    contentDescription = null,
+//                    tint = Color.LightGray
+                )
             }
         }
     )
--- app/src/main/java/com/example/instagram_demo/Component/storybar.kt
+++ app/src/main/java/com/example/instagram_demo/Component/storybar.kt
@@ -5,13 +5,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +32,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instagram_demo.R
 import com.example.instagram_demo.Component.Story
+import com.google.android.gms.tagmanager.Container
 
 @Composable
 @Preview
@@ -35,6 +43,7 @@ fun StoryBar(){
             .background(color = Color.White)
     ) {
         item{
+            MyStory()
             Story()
             Story()
             Story()
@@ -79,5 +88,43 @@ fun Story(){
             )
         }
         Text(text = "Name")
+    }
+}
+
+@Composable
+fun MyStory(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Box{
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(start = 15.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.avatar),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clip(CircleShape)
+
+                )
+            }
+            Box(modifier = Modifier
+                .offset(60.dp, 55.dp)
+            ){
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier
+                            .background(Color(0xff3498db), shape = CircleShape)
+                    )
+                }
+            }
+        }
+        Text(text = "MyName")
     }
 }
